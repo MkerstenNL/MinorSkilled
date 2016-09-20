8c2fb44ab5a2cd529cc9ec5d001556e0c9809a02
MkerstenNL <michael.kersten1994@gmail.com>
2016-09-20 09:42:37 +0200

    old stuff and syntax doc
    old shit

---
+++ Unity Folder/LevelEditingTest/Assets/Scripts/ToolAbstract.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class ToolAbstract : ScriptableObject {
+
+    public abstract void OnEquip();
+    public abstract void OnDequip();
+    public abstract void Activate();
+    public abstract void DeActivate();
+}
