ad96bf05d7decabc078195623e0b09fed08413b0
KingdomHearts <michaelbos@live.nl>
2016-12-12 16:24:22 +0100

    Added the 5 tutorial scenes. And Changed unitychann

---
+++ TestUnity/MichaelsTestProject/NewLuaTesting/Assets/MichaelBossinksTestSceneAssets/Scripts/DontDestroyScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DontDestroyScript : MonoBehaviour {
+
+    private int _level1ElevatorLevel = 1;
+    public int Level1ElevatorLevel { get { return _level1ElevatorLevel; } set { _level1ElevatorLevel = value; } }
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
